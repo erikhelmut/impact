10a9d8074492155005335d8d5df16cae1db95aa0
erikhelmut <erik.helmut1@gmail.com>
2025-03-03 13:31:37 +0100

    fix publisher of marker positions

--- src/impact/impact/realsense_d405/detect_aruco_node.py
+++ src/impact/impact/realsense_d405/detect_aruco_node.py
@@ -28,9 +28,9 @@ class DetectArUcoNode(Node):
         self.subscriber  # prevent unused variable warning
 
         # create publishers for detected markers with id 0 and 1
-        self.marker_publisher = []
-        for m in range(2):
-            self.marker_publisher.append(self.create_publisher(ArUcoMarkerStamped, f"aruco_marker_{m}", 10))
+        self.marker_0_publisher = self.create_publisher(ArUcoMarkerStamped, "aruco_marker_0", 10)
+        self.marker_1_publisher = self.create_publisher(ArUcoMarkerStamped, "aruco_marker_1", 10)
+        self.marker_publisher = {0: self.marker_0_publisher, 1: self.marker_1_publisher}
 
         # create publisher for distance between markers
         self.distance_publisher = self.create_publisher(ArUcoDistStamped, "aruco_distance", 10)
@@ -48,16 +48,19 @@ class DetectArUcoNode(Node):
         :return: distance between two markers and their midpoints
         """
         
-        midp1 = np.mean(corners[ids[0].item()][0], axis=0)
-        midp2 = np.mean(corners[ids[1].item()][0], axis=0)
-        dist = np.linalg.norm(midp1 - midp2)
+        midp0 = np.mean(corners[ids[0].item()][0], axis=0)
+        midp1 = np.mean(corners[ids[1].item()][0], axis=0)
+        dist = np.linalg.norm(midp0 - midp1)
 
-        return dist, midp1, midp2
+        return dist, midp0, midp1
 
 
     def detect_aruco(self, msg):
         """
-        TODO
+        Callback function to detect ArUco markers in the color image.
+
+        :param msg: color image message
+        :return: None
         """
 
         # convert the image message to an OpenCV image
@@ -72,50 +75,58 @@ class DetectArUcoNode(Node):
         # extract header from image message for timestamp
         header = Header()
         header.stamp = msg.header.stamp
-        
-        # TODO: check if it makes sense to hardcode the IDs to 0 and 1
 
-        # check if ids has id 0 and 1
-        if ids is not None and len(ids) == 2:
-        
-            # publish position of markers
-            for id in range(2):
-                aruco_msg = ArUcoMarkerStamped()
-                aruco_msg.header = header
-                aruco_msg.id = id
-                aruco_msg.x = np.array([corners[id][0][0][0], corners[id][0][1][0], corners[id][0][2][0], corners[id][0][3][0]], dtype=np.uint8)
-                aruco_msg.y = np.array([corners[id][0][0][1], corners[id][0][1][1], corners[id][0][2][1], corners[id][0][3][1]], dtype=np.uint8)
-                self.marker_publisher[id].publish(aruco_msg)
-
-            # publish distance between markers
-            dist, midp1, midp2 = self.calculate_distance(corners, ids)
-            dist_msg = ArUcoDistStamped()
-            dist_msg.header = header
-            dist_msg.ids = ids.flatten().astype(np.uint8)
-            dist_msg.distance = dist.item()
-            self.distance_publisher.publish(dist_msg)
-        
-        else:
-            dist = None
+        # publish the position of the detected markers
+        if ids is not None:
+            if len(ids) == 2 and (0 in ids and 1 in ids):
+                # if both markers are detected, publish the position of both markers
+                for id in ids.flatten():
+                    aruco_msg = ArUcoMarkerStamped()
+                    aruco_msg.header = header
+                    aruco_msg.id = int(id)
+                    idx = int(np.where(ids.flatten() == id)[0].item())
+                    aruco_msg.x = np.array([corners[idx][0][0][0], corners[idx][0][1][0], corners[idx][0][2][0], corners[idx][0][3][0]], dtype=np.uint16)
+                    aruco_msg.y = np.array([corners[idx][0][0][1], corners[idx][0][1][1], corners[idx][0][2][1], corners[idx][0][3][1]], dtype=np.uint16)
+                    self.marker_publisher[id].publish(aruco_msg)
+            elif len(ids) == 1 and (0 in ids or 1 in ids):
+                # if only one marker is detected, publish the position of the marker (corners is in this case a tuple with one element)
+                    aruco_msg = ArUcoMarkerStamped()
+                    aruco_msg.header = header
+                    aruco_msg.id = int(ids[0].item())
+                    aruco_msg.x = np.array([corners[0][0][0][0], corners[0][0][1][0], corners[0][0][2][0], corners[0][0][3][0]], dtype=np.uint16)
+                    aruco_msg.y = np.array([corners[0][0][0][1], corners[0][0][1][1], corners[0][0][2][1], corners[0][0][3][1]], dtype=np.uint16)
+                    self.marker_publisher[ids[0].item()].publish(aruco_msg)
 
         # draw the detected markers in the image and publish it
         if ids is not None:
-                
-            aruco_img = cv2.aruco.drawDetectedMarkers(color_img, corners, ids)
+
+            if 0 in ids and 1 in ids:
+                # publish distance between markers
+                dist, midp0, midp1 = self.calculate_distance(corners, ids)
+                dist_msg = ArUcoDistStamped()
+                dist_msg.header = header
+                dist_msg.ids = ids.flatten().astype(np.uint8)
+                dist_msg.distance = dist.item()
+                self.distance_publisher.publish(dist_msg)
+            else:
+                dist = None
+            
+            # draw the detected markers
+            color_img = cv2.aruco.drawDetectedMarkers(color_img, corners, ids)
 
             # draw the line between two markers
             if dist is not None:
-                aruco_img = cv2.line(color_img, (int(midp1[0]), int(midp1[1])), (int(midp2[0]), int(midp2[1])), (0, 255, 0), 2)
+                color_img = cv2.line(color_img, (int(midp0[0]), int(midp0[1])), (int(midp1[0]), int(midp1[1])), (0, 255, 0), 2)
             
-            # publish the image with detected markers
-            image_msg = Image()
-            image_msg.header = header
-            image_msg.data = aruco_img.tobytes()
-            image_msg.height = aruco_img.shape[0]
-            image_msg.width = aruco_img.shape[1]
-            image_msg.encoding = "bgr8"
-            image_msg.step = aruco_img.shape[1] * 3
-            self.image_publisher.publish(image_msg)
+        # publish the image with detected markers and distance (if available)
+        image_msg = Image()
+        image_msg.header = header
+        image_msg.data = color_img.tobytes()
+        image_msg.height = color_img.shape[0]
+        image_msg.width = color_img.shape[1]
+        image_msg.encoding = "bgr8"
+        image_msg.step = color_img.shape[1] * 3
+        self.image_publisher.publish(image_msg)
 
 
 def main(args=None):
@@ -126,6 +137,18 @@ def main(args=None):
     """
 
     try:
+
+        print("""
+        DETECT ARUCO
+         ██████████
+         █ ██  █  █
+         █  ████ ██
+         █ █  ██  █
+         █ ██  ████
+         ██████████
+        """)
+
+        print("Detect ArUco Node is running... Press <ctrl> <c> to stop. \nPosition of ArUco markers with id 0 and 1 are being published on topics /aruco_marker_0 and /aruco_marker_1. Distance between markers is being published on topic /aruco_distance. Image with detected markers and distance is being published on topic /realsense_d405_color_image_aruco. \n")
 
         rclpy.init(args=args)
 
