83e93513ac651f2f42639199d6d31031ca993093
erikhelmut <erik.helmut1@gmail.com>
2025-03-18 15:52:13 +0100

    change video capture to ffmpeg

--- src/gelsight_mini/gelsight_mini/gelsight_mini_node.py
+++ src/gelsight_mini/gelsight_mini/gelsight_mini_node.py
@@ -1,11 +1,213 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Header
 from sensor_msgs.msg import Image
+
+import threading
 
 import cv2
 from cv_bridge import CvBridge
+import ffmpegcv
+from ffmpegcv.ffmpeg_reader_camera import query_camera_devices
 import numpy as np
+
+
+class GelSightSensorError(RuntimeError):
+    pass
+
+
+class GelSight:
+    """
+    Class to connect to the GelSight sensor using the ffmpegcv package.
+    Credit to Janis Lenz, Alina Böhm, Inga Pfenning for the code.
+    """
+
+    def __init__(self, img_shape: tuple[int, int] = (480, 320)):
+        """
+        Initialize connection to the GelSight sensor using the ffmpegcv package.
+
+        :param img_shape: shape of the image (default: (480, 320))
+        :return: None
+        """
+
+        self.__camera_name: str = "GelSight"
+        self.__img_shape = img_shape
+        self.__latest_img = np.zeros(tuple(reversed(img_shape)) + (3,), dtype=np.uint8)
+        self.__img_before_latest = np.zeros(
+            tuple(reversed(img_shape)) + (3,), dtype=np.uint8
+        )
+        self.__camera = ffmpegcv.VideoCaptureCAM(
+            self._get_camera_id(), pix_fmt="rgb24", resize=self.__img_shape
+        )
+        if not self.connected:
+            pass
+
+        self.terminate_thread = False
+        self.lock = threading.Lock()
+        self.thread = threading.Thread(
+            target=self._image_thread, name=f"{self.__camera_name}_thread"
+        )
+        self.thread.start()
+
+
+    def __del__(self):
+        """
+        Destructor for the GelSight sensor.
+
+        :return: None
+        """
+
+        self.close()
+
+
+    def close(self):
+        """
+        Close the connection to the GelSight.
+        
+        :return: None
+        """
+        
+        self.terminate_thread = True
+        self.thread.join()
+        self.__camera.close()
+
+
+    def _image_thread(self):
+        """
+        Thread function to receive the images asynchronously.
+
+        :return: None
+        """
+        
+        try:
+            while not self.terminate_thread:
+                success, img = self.__camera.read()
+                if not success:
+                    print(f"{self.__camera_name} thread closed!")
+                    self.terminate_thread = True
+                    continue
+                with self.lock:
+                    self.__latest_img = img
+                    self.__img_before_latest = self.__latest_img
+
+        except Exception as ex:
+            pass
+
+
+    def _get_camera_id(self):
+        """
+        Determine camera ID of the GelSight.
+
+        :raises: GelSightSensorError: ff no camera could be found
+        :return: int: ID of the camera
+        """
+
+        devices = query_camera_devices()
+        camera_ids = list(devices.keys())
+
+        for id in camera_ids:
+            name = str(devices[id][0])
+            if self.__camera_name.lower() in name.lower():  # (id != 0) and
+                print(f"Found {self.__camera_name}: {devices[id]}, id: {id}")
+                return id
+            
+        raise GelSightSensorError(f"Could not find {self.__camera_name}!")
+
+
+    def __enter__(self):
+        """
+        Context manager to use the GelSight sensor.
+
+        :return: self
+        """
+
+        return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Context manager to close the connection to the GelSight sensor.
+
+        :param exc_type: exception type
+        :param exc_val: exception value
+        :param exc_tb: exception traceback
+        :return: None
+        """
+
+        self.close()
+
+
+    @property
+    def camera_name(self):
+        """
+        Camera name.
+
+        :return: string: camera name
+        """
+
+        return self.__camera_name
+
+
+    @property
+    def camera(self) -> ffmpegcv.FFmpegReaderCAM:
+        """
+        Camera object.
+
+        :return: ffmpegcv.FFmpegReaderCAM: camera object
+        """
+        
+        return self.__camera
+
+
+    @property
+    def connected(self):
+        """
+        Connected to the the GelSight.
+
+        :return: bool: true if connected, otherwise false
+        """
+        
+        return self.__camera is not None and self.__camera.isOpened()
+
+
+    @property
+    def img_size(self) -> tuple[int, int]:
+        """
+        Shape of the image.
+
+        :return: tuple[int, int]: shape
+        """
+
+        return self.__img_shape
+
+
+    @property
+    def latest_img(self) -> cv2.typing.MatLike:
+        """
+        Latest captured image of the GelSight sensor.
+
+        :raises: GelSightSensorError: if connection lost
+        :return: cv2.typing.MatLike: latest image
+        """
+
+        if not self.connected and not self.thread.is_alive():
+            raise GelSightSensorError(f"Connection to {self.__camera_name} lost!")
+        
+        return self.__latest_img
+
+
+    @property
+    def second_latest_img(self) -> cv2.typing.MatLike:
+        """
+        Second latest captured image of the GelSight sensor.
+
+        :raises: GelSightSensorError: if connection lost
+        :return: cv2.typing.MatLike: second latest image
+        """
+
+        if not self.connected and not self.thread.is_alive():
+            raise GelSightSensorError(f"Connection to {self.__camera_name} lost!")
+        
+        return self.__img_before_latest
 
 
 class GelSightMiniNode(Node):
@@ -20,7 +222,7 @@ class GelSightMiniNode(Node):
         super().__init__("gelsight_mini_node")
 
         # initialize camera
-        self.cam = cv2.VideoCapture(0)
+        self.cam= GelSight(img_shape=(895, 672)) # size suggested by janos to maintain aspect ratio
 
         # set camera parameters
         self.imgw = 320
@@ -42,24 +244,19 @@ class GelSightMiniNode(Node):
         :return: None
         """
 
-        # read image from camera
-        ret, f0 = self.cam.read()
+        # get latest image from camera
+        img = cv2.cvtColor(self.cam.latest_img, cv2.COLOR_BGR2RGB)
 
-        if ret:
-            # resize, crop and resize back
-            img = cv2.resize(f0, (895, 672))  # size suggested by janos to maintain aspect ratio
-            border_size_x, border_size_y = int(img.shape[0] * (1 / 7)), int(np.floor(img.shape[1] * (1 / 7)))  # remove 1/7th of border from each size
-            img = img[border_size_x:img.shape[0] - border_size_x, border_size_y:img.shape[1] - border_size_y]
-            img = img[:, :-1]  # remove last column to get a popular image resolution
-            img = cv2.resize(img, (self.imgw, self.imgh))  # final resize for 3d
+        # crop and resize
+        border_size_x, border_size_y = int(img.shape[0] * (1 / 7)), int(np.floor(img.shape[1] * (1 / 7)))  # remove 1/7th of border from each size
+        img = img[border_size_x:img.shape[0] - border_size_x, border_size_y:img.shape[1] - border_size_y]
+        img = img[:, :-1]  # remove last column to get a popular image resolution
+        img = cv2.resize(img, (self.imgw, self.imgh))  # final resize for 3d
 
-            # convert to ros msg image
-            img_msg = self.bridge.cv2_to_imgmsg(img, "bgr8")
-            img_msg.header.stamp = self.get_clock().now().to_msg()
-            self.gs_mini_publisher_.publish(img_msg)
-
-        else:
-            print("ERROR! reading image from camera")
+        # convert to ros msg image
+        img_msg = self.bridge.cv2_to_imgmsg(img, "bgr8")
+        img_msg.header.stamp = self.get_clock().now().to_msg()
+        self.gs_mini_publisher_.publish(img_msg)
 
         
 def main(args=None):
