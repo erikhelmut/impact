70c4d8fd7dd01fbed0aa5e43814905abb5c4cd5a
erikhelmut <erik.helmut1@gmail.com>
2025-03-25 16:48:09 +0100

    create empty package for feats

---
+++ src/feats/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = 'feats'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='erik',
+    maintainer_email='erik.helmut1@gmail.com',
+    description='FEATS ROS2 Jazzy Package',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
