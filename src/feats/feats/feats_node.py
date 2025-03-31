1c3e58cae78ca82cb6ff4b59ab0f0ad9be1d5a50
erikhelmut <erik.helmut1@gmail.com>
2025-03-31 16:33:03 +0200

    implement publisher for force distributions and total forces

--- src/feats/feats/feats_node.py
+++ src/feats/feats/feats_node.py
@@ -1,244 +1,237 @@
+#import sys; sys.path.append("../")
+
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 
-import sys; sys.path.append("../")
-import argparse
 import yaml
 
 import cv2
+from cv_bridge import CvBridge
 import numpy as np
 import torch
 
 from feats.dataloader import normalize, unnormalize
 from feats.unet import UNet
+from feats_msgs.msg import ForceDistStamped
 
 
 class FEATS:
 
     def __init__(self):
-        pass
+        """
+        This class is used to make predictions using the FEATS model. It loads the model and the configuration file, and sets the device to GPU if available.
 
-    def make_prediction(self):
-        pass
+        :return: None
+        """
+        
+        # specify device
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        else:
+            self.device = torch.device("cpu")
 
+        # load config file
+        self.config = yaml.safe_load(open("../config/predict_config.yaml", "r"))
+
+        # load model
+        self.model = UNet(enc_chs=self.config["enc_chs"], dec_chs=self.config["dec_chs"], out_sz=self.config["output_size"])
+        self.model.load_state_dict(torch.load(self.config["model"], map_location=torch.device("cpu"), weights_only=True))
+        self.model.eval().to(self.device)
+
+
+    def make_prediction(self, img):
+        """
+        Make prediction using the FEATS model.
+
+        :param img: current image of the gelsight mini
+        :return: pred_grid_x, pred_grid_y, pred_grid_z: predicted force distributions
+        """
+
+        # store data in dictionary
+        data = {}
+        data["gs_img"] = img
+        
+        # normalize data
+        data = normalize(data, self.config["norm_file"])
+
+        # convert to torch tensor
+        gs_img = torch.from_numpy(data["gs_img"]).float()
+        gs_img = gs_img.unsqueeze(0).permute(0, 3, 1, 2).to(self.device)
+
+        # load calibration file
+        if self.config["calibration_file"] is not None:
+            calibration = np.load(self.config["calibration_file"])
+            rows, cols = 240, 320
+            M = np.float32([[1, 0, calibration[0]], [0, 1, calibration[1]]])
+
+        # prepare input data
+        if self.config["calibration_file"] is not None:
+            inputs_prewarp = data["gs_img"]
+            inputs_warp = cv2.warpAffine(inputs_prewarp,  M, (cols, rows), borderMode=cv2.BORDER_REPLICATE)
+            inputs = torch.from_numpy(inputs_warp).permute(2, 0, 1).unsqueeze(0).float().to(self.device)
+        else:
+            inputs = gs_img
+
+        # get model prediction
+        outputs = self.model(inputs)
+
+        # unnormalize the outputs
+        outputs_transf = outputs.squeeze(0).permute(1, 2, 0)
+        pred_grid_x = unnormalize(outputs_transf[:, :, 0], "grid_x", self.config["norm_file"])
+        pred_grid_y = unnormalize(outputs_transf[:, :, 1], "grid_y", self.config["norm_file"])
+        pred_grid_z = unnormalize(outputs_transf[:, :, 2], "grid_z", self.config["norm_file"])
+
+        # convert to numpy
+        pred_grid_x = pred_grid_x.cpu().detach().numpy()
+        pred_grid_y = pred_grid_y.cpu().detach().numpy()
+        pred_grid_z = pred_grid_z.cpu().detach().numpy()
+
+        return pred_grid_x, pred_grid_y, pred_grid_z
 
 
 class FEATSNode(Node):
 
     def __init__(self):
+        """
+        This node subscribes to the current image of the gelsight mini and makes predictions using the FEATS model. It publishes the predicted force distribution as an image.
+
+        :return: None
+        """
         
         super().__init__("feats_node")
 
+        # initialize FEATS model
+        self.feats = FEATS()
+
+        # define color limits
+        self.clim_x = (-0.029, 0.029)
+        self.clim_y = (-0.029, 0.029)
+        self.clim_z = (-0.17, 0.0)
+
         # create subscriber to get current image of the gelsight mini
-        self.gelsight_subscriber = self.create_subscription(Image, "gelsight_mini_image", self.make_prediction, 10)
+        self.gelsight_subscriber = self.create_subscription(Image, "gelsight_mini_image", self.pub_prediction, 10)
         self.gelsight_subscriber  # prevent unused variable warning
 
-        # create publisher for the FEATS
-        self.feats_publisher_ = self.create_publisher(Image, "feats_image", 10)
-        timer_period = 1.0 / 25  # 25 Hz
+        # create publisher for FEATS
+        self.feats_publisher_ = self.create_publisher(ForceDistStamped, "feats", 10)
+        self.feats_x_publisher_ = self.create_publisher(Image, "feats_fx_image", 10)
+        self.feats_y_publisher_ = self.create_publisher(Image, "feats_fy_image", 10)
+        self.feats_z_publisher_ = self.create_publisher(Image, "feats_fz_image", 10)
 
 
-def capture_image(cam, imgw=320, imgh=240):
+    def pub_prediction(self, msg):
+        """
+        Publish the prediction of the FEATS model.
+
+        :param msg: current image of the gelsight mini
+        :return: None
+        """
+
+        # convert ros image message to numpy array
+        img = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, 3)
+
+        # make prediction with FEATS model
+        pred_grid_x, pred_grid_y, pred_grid_z = self.feats.make_prediction(img)
+
+        # calculate total forces
+        f_x = np.sum(pred_grid_x)
+        f_y = np.sum(pred_grid_y)
+        f_z = np.sum(pred_grid_z)
+
+        # create general Image message structure
+        
+        
+        # create Image message for x
+        pred_grid_x = (pred_grid_x - self.clim_x[0]) / (self.clim_x[1] - self.clim_x[0])
+        pred_grid_x = np.clip(pred_grid_x, 0, 1) * 255
+        pred_grid_x = np.uint8(pred_grid_x)
+        pred_grid_x = cv2.applyColorMap(pred_grid_x, cv2.COLORMAP_VIRIDIS)
+        pred_grid_x = cv2.resize(pred_grid_x, (320, 240), interpolation=cv2.INTER_NEAREST)
+
+        pred_grid_x_msg = Image()
+        pred_grid_x_msg.header = msg.header
+        pred_grid_x_msg.data = pred_grid_x.tobytes()
+        pred_grid_x_msg.height = pred_grid_x.shape[0]
+        pred_grid_x_msg.width = pred_grid_x.shape[1]
+        pred_grid_x_msg.encoding = "bgr8"
+        pred_grid_x_msg.step = pred_grid_x.shape[1] * 3
+        pred_grid_x_msg.header.frame_id = "feats_fx_image"
+        self.feats_x_publisher_.publish(pred_grid_x_msg)
+        
+        # create Image message for y
+        pred_grid_y = (pred_grid_y - self.clim_y[0]) / (self.clim_y[1] - self.clim_y[0])
+        pred_grid_y = np.clip(pred_grid_y, 0, 1) * 255
+        pred_grid_y = np.uint8(pred_grid_y)
+        pred_grid_y = cv2.applyColorMap(pred_grid_y, cv2.COLORMAP_VIRIDIS)
+        pred_grid_y = cv2.resize(pred_grid_y, (320, 240), interpolation=cv2.INTER_NEAREST)
+        
+        pred_grid_y_msg = Image()
+        pred_grid_y_msg.header = msg.header
+        pred_grid_y_msg.data = pred_grid_y.tobytes()
+        pred_grid_y_msg.height = pred_grid_y.shape[0]
+        pred_grid_y_msg.width = pred_grid_y.shape[1]
+        pred_grid_y_msg.encoding = "bgr8"
+        pred_grid_y_msg.step = pred_grid_y.shape[1] * 3
+        pred_grid_y_msg.header.frame_id = "feats_fy_image"
+        self.feats_y_publisher_.publish(pred_grid_y_msg)
+
+        # create Image message for z
+        pred_grid_z = (pred_grid_z - self.clim_z[0]) / (self.clim_z[1] - self.clim_z[0])
+        pred_grid_z = np.clip(pred_grid_z, 0, 1) * 255
+        pred_grid_z = np.uint8(pred_grid_z)
+        pred_grid_z = cv2.applyColorMap(pred_grid_z, cv2.COLORMAP_VIRIDIS)
+        pred_grid_z = cv2.resize(pred_grid_z, (320, 240), interpolation=cv2.INTER_NEAREST)
+
+        pred_grid_z_msg = Image()
+        pred_grid_z_msg.header = msg.header
+        pred_grid_z_msg.data = pred_grid_z.tobytes()
+        pred_grid_z_msg.height = pred_grid_z.shape[0]
+        pred_grid_z_msg.width = pred_grid_z.shape[1]
+        pred_grid_z_msg.encoding = "bgr8"
+        pred_grid_z_msg.step = pred_grid_z.shape[1] * 3
+        pred_grid_z_msg.header.frame_id = "feats_fz_image"
+        self.feats_z_publisher_.publish(pred_grid_z_msg)
+        
+        # create ForceDistStamped message
+        feats_msg = ForceDistStamped()
+        feats_msg.header = msg.header
+        feats_msg.fd_x = pred_grid_x.flatten().tolist()
+        feats_msg.fd_y = pred_grid_y.flatten().tolist()
+        feats_msg.fd_z = pred_grid_z.flatten().tolist()
+        feats_msg.f_x = f_x.item()
+        feats_msg.f_y = f_y.item()
+        feats_msg.f_z = f_z.item()
+        self.feats_publisher_.publish(feats_msg)
+
+
+def main(args=None):
     """
-    Capture image from GelSight Mini and process it.
+    ROS node for making live predictions using the FEATS model.
 
-    :param cam: video capture object
-    :param imgw: width of the image
-    :param imgh: height of the image
-    :return: processed image
-    """
-
-    # capture image
-    ret, f0 = cam.read()
-
-    if ret:
-        # resize, crop and resize back
-        img = cv2.resize(f0, (895, 672))  # size suggested by janos to maintain aspect ratio
-        border_size_x, border_size_y = int(img.shape[0] * (1 / 7)), int(np.floor(img.shape[1] * (1 / 7)))  # remove 1/7th of border from each size
-        img = img[border_size_x:img.shape[0] - border_size_x, border_size_y:img.shape[1] - border_size_y]
-        img = img[:, :-1]  # remove last column to get a popular image resolution
-        img = cv2.resize(img, (imgw, imgh))  # final resize for 3d
-    else:
-        print("ERROR! reading image from camera")
-
-    # convert bgr to rgb
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    return img
-
-
-def make_prediction(img, model, device, config):
-    """
-    Make prediction using the unet model.
-
-    :param img: input image
-    :param model: unet model
-    :param device: device to run the model
-    :param config: configuration file
-    :return: predicted grid x, y, z
-    """
-
-    # store data in dictionary
-    data = {}
-    data["gs_img"] = img
-
-    # normalize data
-    data = normalize(data, config["norm_file"])
-
-    # convert to torch tensor
-    gs_img = torch.from_numpy(data["gs_img"]).float()
-    gs_img = gs_img.unsqueeze(0).permute(0, 3, 1, 2).to(device)
-
-    # load calibration file
-    if config["calibration_file"] is not None:
-        calibration = np.load(config["calibration_file"])
-        rows, cols = 240, 320
-        M = np.float32([[1, 0, calibration[0]], [0, 1, calibration[1]]])
-
-    # prepare input data
-    if config["calibration_file"] is not None:
-        inputs_prewarp = data["gs_img"]
-        inputs_warp = cv2.warpAffine(inputs_prewarp,  M, (cols, rows), borderMode=cv2.BORDER_REPLICATE)
-        inputs = torch.from_numpy(inputs_warp).permute(2, 0, 1).unsqueeze(0).float().to(device)
-    else:
-        inputs = gs_img
-
-    # get model prediction
-    outputs = model(inputs)
-
-    # unnormalize the outputs
-    outputs_transf = outputs.squeeze(0).permute(1, 2, 0)
-    pred_grid_x = unnormalize(outputs_transf[:, :, 0], "grid_x", config["norm_file"])
-    pred_grid_y = unnormalize(outputs_transf[:, :, 1], "grid_y", config["norm_file"])
-    pred_grid_z = unnormalize(outputs_transf[:, :, 2], "grid_z", config["norm_file"])
-
-    # convert to numpy
-    pred_grid_x = pred_grid_x.cpu().detach().numpy()
-    pred_grid_y = pred_grid_y.cpu().detach().numpy()
-    pred_grid_z = pred_grid_z.cpu().detach().numpy()
-
-    return pred_grid_x, pred_grid_y, pred_grid_z
-
-
-def animate(frame, cam, model, device, config, ims, axs):
-    """
-    Animate the prediction with matplotlib.
-
-    :param frame: frame number
-    :param cam: camera object
-    :param mdoel: unet model
-    :param device: device to run the model
-    :param config: configuration dictionary
-    :param ims: image objects
-    :param axs: axis objects
+    :param args: arguments for the ROS node
     :return: None
     """
 
-    # capture image and make prediction
-    gs_img = capture_image(cam)
-    pred_grid_x, pred_grid_y, pred_grid_z = make_prediction(gs_img, model, device, config)
+    try:
 
-    # update the image data
-    ims[0].set_data(pred_grid_x)
-    ims[1].set_data(pred_grid_y)
-    ims[2].set_data(pred_grid_z)
-    ims[3].set_data(gs_img.astype(np.uint8))
+        print("FEATS Node is running... Press <ctrl> <c> to stop. \nPredicted force distributions are being published on topic /feats or /feats_fX_image. \n")
 
-    # update titles with the new force values
-    axs[0].set_title("x-force: {:.2f}N".format(np.sum(pred_grid_x)), fontsize=14)
-    axs[1].set_title("y-force: {:.2f}N".format(np.sum(pred_grid_y)), fontsize=14)
-    axs[2].set_title("z-force: {:.2f}N".format(np.sum(pred_grid_z)), fontsize=14)
+        rclpy.init(args=args)
 
-    return None
+        feats_node = FEATSNode()
 
+        rclpy.spin(feats_node)
 
-def main(config):
-    """
-    Main function to run the live prediction.
+    finally:
 
-    :param config: configuration dictionary containing model parameters and paths
-    :return: None
-    """
-
-    # initialize camera
-    cam = cv2.VideoCapture(0)
-
-    # specify device
-    if torch.backends.mps.is_available():
-        device = torch.device("mps")
-    else:
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # load model
-    model = UNet(enc_chs=config["enc_chs"], dec_chs=config["dec_chs"], out_sz=config["output_size"])
-    model.load_state_dict(torch.load(config["model"], map_location=torch.device("cpu"), weights_only=True))
-    model.eval().to(device)
-
-    # Set up the figure and initial images
-    fig, axs = plt.subplots(1, 4, figsize=(16, 5))
-    fig.canvas.manager.set_window_title("FEATS")
-    fig.suptitle("\nFEATS LIVE DEMO", fontsize=16)
-
-    # reduce whitespace of plot
-    plt.subplots_adjust(left=0.05, right=0.95, top=2.7, bottom=0.1)
-
-    # capture first image of the camera
-    gs_img = capture_image(cam)
-
-    # make initial prediction
-    pred_grid_x, pred_grid_y, pred_grid_z = make_prediction(gs_img, model, device, config)
-
-    # hardcode the color limits
-    clim_x = (-0.029, 0.029)
-    clim_y = (-0.029, 0.029)
-    clim_z = (-0.17, 0.0)
-
-    # initialize the plots
-    im_x = axs[0].imshow(pred_grid_x, origin="upper", vmin=clim_x[0], vmax=clim_x[1], animated=True)
-    im_y = axs[1].imshow(pred_grid_y, origin="upper", vmin=clim_y[0], vmax=clim_y[1], animated=True)
-    im_z = axs[2].imshow(pred_grid_z, origin="upper", vmin=clim_z[0], vmax=clim_z[1], animated=True)
-    im_gs = axs[3].imshow(gs_img.astype(np.uint8), animated=True)
-
-    # store the images in a list
-    ims = [im_x, im_y, im_z, im_gs]
-
-    # set titles
-    axs[0].set_title("x-force: {:.2f}N".format(np.sum(pred_grid_x)), fontsize=14)
-    axs[1].set_title("y-force: {:.2f}N".format(np.sum(pred_grid_y)), fontsize=14)
-    axs[2].set_title("z-force: {:.2f}N".format(np.sum(pred_grid_z)), fontsize=14)
-    axs[3].set_title("GelSight Mini Image", fontsize=14)
-
-    # turn off axis
-    axs[0].axis("off")
-    axs[1].axis("off")
-    axs[2].axis("off")
-    axs[3].axis("off")
-
-    # show colorbars
-    fig.colorbar(im_x, ax=axs[0], orientation="horizontal", fraction=0.046, pad=0.01, ticks=[-0.02, -0.01, 0.0, 0.01, 0.02])
-    fig.colorbar(im_y, ax=axs[1], orientation="horizontal", fraction=0.046, pad=0.01, ticks=[-0.02, -0.01, 0.0, 0.01, 0.02])
-    fig.colorbar(im_z, ax=axs[2], orientation="horizontal", fraction=0.046, pad=0.01, ticks=[-0.15, -0.1, -0.05, 0.0])
-
-    # add fake colorbar for axs[3] so that gs_img is on the same height
-    fake_cbar = fig.colorbar(im_gs, ax=axs[3], orientation="horizontal", fraction=0.046, pad=0.01)
-    fake_cbar.ax.set_visible(False)
-
-    # animate the predictions
-    ani = FuncAnimation(fig, animate, frames=None, interval=0, blit=False, fargs=(cam, model, device, config, ims, axs))
-
-    plt.show()
+        # Destroy the node explicitly
+        # (optional - otherwise it will be done automatically
+        # when the garbage collector destroys the node object)
+        feats_node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
 
-    # parse arguments
-    parser = argparse.ArgumentParser(description="Live prediction of UNet")
-    parser.add_argument("-c", "--config", type=str, default="predict_config.yaml", help="Path to config file for live prediction.")
-    args = parser.parse_args()
-
-    # load config file
-    config = yaml.load(open(args.config, "r"), Loader=yaml.FullLoader)
-
-    # make live prediction
-    main(config)
+    main()
