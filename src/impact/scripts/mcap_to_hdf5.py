c40bf5edc2a0c29484d224486116fa0bd5ee8592
erikhelmut <erik.helmut1@gmail.com>
2025-04-09 16:33:41 +0200

    start working on mcap to hdf5 converter

---
+++ src/impact/scripts/mcap_to_hdf5.py
@@ -0,0 +1,92 @@
+import argparse
+from rclpy.serialization import deserialize_message
+from rosidl_runtime_py.utilities import get_message
+from std_msgs.msg import Int16
+import rosbag2_py
+
+
+class RosbagReader():
+
+    def __init__(self, input_bag: str):
+        """
+        Rosbag reader class to read messages from a bag file.
+
+        :param input_bag: path to the bag file
+        :return: None
+        """
+        
+        self.reader = rosbag2_py.SequentialReader()
+        self.reader.open(
+            rosbag2_py.StorageOptions(uri=input_bag, storage_id="mcap"),
+            rosbag2_py.ConverterOptions(
+                input_serialization_format="cdr", output_serialization_format="cdr"
+            ),
+        )
+
+        self.topic_types = self.reader.get_all_topics_and_types()
+
+
+    def __del__(self):
+        """
+        Destructor to close the reader object.
+        
+        :return: None
+        """
+
+        del self.reader
+
+
+    def typename(self, topic_name):
+        """
+        Get the message type of a topic.
+
+        :param topic_name: name of the topic
+        :return: message type of the topic
+        :raises ValueError: if the topic is not in the bag
+        """
+        
+        for topic_type in self.topic_types:
+            if topic_type.name == topic_name:
+                return topic_type.type
+        
+        raise ValueError(f"topic {topic_name} not in bag")
+
+
+    def read_messages(self):
+        """
+        Generator function to read messages from the bag file.
+
+        :return: topic name, message, timestamp
+        """
+
+        while self.reader.has_next():
+            topic, data, timestamp = self.reader.read_next()
+            msg_type = get_message(self.typename(topic))
+            msg = deserialize_message(data, msg_type)
+            
+            yield topic, msg, timestamp
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--input", help="input bag path (folder or filepath) to read from"
+    )
+    args = parser.parse_args()
+
+    # create reader object
+    rr = RosbagReader(args.input)
+
+    for topic, msg, timestamp in rr.read_messages():
+        if isinstance(msg, Int16):
+            print(f"{topic} [{timestamp}]: '{msg.data}'")
+        else:
+            print(f"{topic} [{timestamp}]: ({type(msg).__name__})")
+
+        input()
+   
+
+if __name__ == "__main__":
+
+    main()
